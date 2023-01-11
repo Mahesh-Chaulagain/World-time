4ae78937a6f9124c94b409dafdc428a3c5b7dc38
Mahesh-Chaulagain <maheshchaulagain8@gmail.com>
2023-01-11 13:34:22 +0545

    Added images and API integration

---
+++ lib/pages/loading.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(
+        location: 'Kathmandu', flag: 'nepal.png', urls: 'Asia/Kathmandu');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDaytime
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue,
+        body: Center(
+            child: SpinKitCubeGrid(
+          size: 50,
+          color: Colors.white,
+        )));
+  }
+}
